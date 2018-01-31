6a75bcb15a74ce61b87e9561f2d6a5acdb2065b1
Vitalii <27superuser@gmail.com>
2018-01-31 08:16:45 +0200

    DiagnosisDTO and IDiagnosisService added

---
+++ Server/BLL/Interfaces/IDiagnosisService.cs
@@ -0,0 +1,15 @@
+﻿using BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Interfaces
+{
+    public interface IDiagnosisService
+    {
+        void CreateDiagnosis(DiagnosisDTO diagnosisDTO);
+        DiagnosisDTO GetDiagnosisById(int? id);
+        IEnumerable<DiagnosisDTO> GetDiagnosis();
+        void Dispose();
+    }
+}
--- Server/IdentityServer/Startup.cs
+++ Server/IdentityServer/Startup.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using BLL.Interfaces;
-using BLL.Services;
 using DAL.EF;
 using DAL.Interfaces;
 using DAL.Repositories;

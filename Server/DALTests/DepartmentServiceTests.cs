6f919aef90ce968191c9bc96ca88d0b94b3b6109
Yulia <y.u.l.k.a._ua@ukr.net>
2018-02-01 15:07:04 +0200

    Added TEST

--- Server/DALTests/DepartmentServiceTests.cs
+++ Server/DALTests/DepartmentServiceTests.cs
@@ -84,7 +84,7 @@ namespace DALTests
             var result = users.Where(x => x.Id == id).FirstOrDefault();
             
             return result;
-            //test
+            
         }
     }
 }

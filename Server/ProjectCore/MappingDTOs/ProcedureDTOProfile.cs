c9df4764f7d82c8610a1a6e083bfe163af155131
Roman Nesteryk <rnesteryk@gmail.com>
2018-02-07 22:11:24 +0200

    Added MapperProfile for all DTOs

---
+++ Server/ProjectCore/MappingDTOs/ProcedureDTOProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjectCore.DTO;
+using ProjectCore.Entities;
+
+namespace ProjectCore.MappingDTOs
+{
+    public class ProcedureDTOProfile : Profile
+    {
+        public ProcedureDTOProfile()
+        {
+            CreateMap<Procedure, ProcedureDTO>();
+            CreateMap<ProcedureDTO, Procedure>();
+        }
+    }
+}
---
+++ Server/ProjectCore/MappingDTOs/UserDTOProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjectCore.DTO;
+using ProjectCore.Entities;
+
+namespace ProjectCore.MappingDTOs
+{
+    public class UserDTOProfile : Profile
+    {
+        public UserDTOProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+        }
+    }
+}

5db81f8113bde92c282d7c9ba807769d240f8781
DoubleRom082 <DoubleRom082@gmail.com>
2018-02-16 12:46:41 +0200

    Add PrescriptionServiceTests, PrescriptionListServiceTests

---
+++ Server/BLL.Tests/Stubs/StubData/PrescriptionUowStubData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectCore.Entities;
+
+namespace BLL.Tests.Stubs.StubData
+{
+    public static class PrescriptionUowStubData
+    {
+        private static List<Prescription> _prescriptions;
+
+        public static List<Prescription> Prescriptions
+        {
+            get
+            {
+                if (_prescriptions == null)
+                {
+                    _prescriptions = new List<Prescription>
+                    {
+                        new Prescription{ Id = 101, DoctorId = 1, PatientId = 1, Description = "tablets",
+                            Date = DateTime.Now, DiagnosisId = 1},
+                        new Prescription{ Id = 102, DoctorId = 1, PatientId = 1, Description = "tea",
+                            Date = DateTime.Now, DiagnosisId = 2},
+                        new Prescription{ Id = 103, DoctorId = 1, PatientId = 1, Description = "nimesil",
+                            Date = DateTime.Now, DiagnosisId = 3},
+                    };
+                }
+                return _prescriptions;
+            }
+        }
+    }
+}
---
+++ Server/BLL.Tests/Stubs/StubServices/PrescriptionUowStubService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Tests.Stubs.StubData;
+using ProjectCore.Entities;
+
+namespace BLL.Tests.Stubs.StubServices
+{
+    public static class PrescriptionUowStubService
+    {
+        private static List<Prescription> _prescriptions = PrescriptionUowStubData.Prescriptions;
+
+        public static async Task<IList<Prescription>> GetAll()
+        {
+            await Task.Yield();
+            return _prescriptions;
+        }
+
+        public static async Task<Prescription> GetById(int id)
+        {
+            await Task.Yield();
+            var prescription = _prescriptions.FirstOrDefault(u => u.Id == id);
+            return prescription;
+        }
+    }
+}

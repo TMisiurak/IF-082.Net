05f91cd3cd6a758226faf4d3f01c1f9d4764c29e
PodoliankoRoman <routtor@gmail.com>
2018-02-21 20:53:28 +0200

    Change Schedule

--- Server/DAL/Repositories/EFRepositories/ScheduleRepository.cs
+++ Server/DAL/Repositories/EFRepositories/ScheduleRepository.cs
@@ -5,6 +5,7 @@ using ProjectCore.Entities;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DAL.Repositories.EFRepositories
@@ -25,9 +26,18 @@ namespace DAL.Repositories.EFRepositories
 
         public async Task<Schedule> GetById(int id)
         {
-            var param = new SqlParameter("@Id", id);
-            Schedule user = await _db.Schedules.FromSql($"sp_GetScheduleById @Id", param).FirstOrDefaultAsync();
-            return user;
+            var param = new SqlParameter("@DoctorId", id);
+            var param1 = new SqlParameter("@Id", id);
+            var x = await _db.Schedules.FromSql($"sp_GetScheduleByDoctorId @DoctorId", param).ToListAsync();
+            var y = await _db.Appointments.FromSql($"sp_GetAppointmentById @Id", param1).ToListAsync();
+            var result = x.Join(y, xs => xs.Weekday, ys => (int)ys.Date.DayOfWeek, (xs, ys) => new { A = xs.SlotDuration, B = xs.Weekday });
+            foreach (var i in result)
+            {
+                System.Console.WriteLine("{0} - {1}", i.A, i.B);
+            }
+
+            //return user;
+            return new Schedule();
         }
 
         public async Task<int> Create(Schedule schedule)
